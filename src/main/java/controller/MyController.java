5f89d5c1f0f44de8618cbd53f71d82e19efd86a1
chen <@DESKTOP-SB413GK>
2018-04-02 00:02:03 +0800

    第二次作业

--- src/main/java/controller/MyController.java
+++ src/main/java/controller/MyController.java
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MyController {
      @RequestMapping("/first")
+     //里面的first指的是浏览器搜索的名字
      public String getFirst() {
     	 return "firstpage";
+     //firstpage是实际搜索的jsp
      }
 }
 
---
+++ src/main/java/controller/PojoController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PojoController {
+	 @RequestMapping(value="/pojo1")
+     public String mypojo1(Order order) {
+  	   System.out.println("oaddress:"+order.getOaddress());
+  	   System.out.println("odate:"+order.getOdate());
+  	   return "order";
+  	   //order是实际指向的jsp
+     }
+	 
+  
+     @RequestMapping(value="/pojo2")
+     public String mypojo2(Order order) {
+  	   User u =order.getUser();
+  	   System.out.println("oaddress:"+order.getOaddress());
+  	   System.out.println("uname:"+u.getUname());
+  	   return "order2";
+     }
+}
